270a41c3df3d2ceff2e3b807ec1ceb572c130abf
tittioAngel <44965659+tittioAngel@users.noreply.github.com>
2021-06-25 19:46:30 +0200

    nomi film messi mella lista

--- src/gestionecinema/GestioneCinema.java
+++ src/gestionecinema/GestioneCinema.java
@@ -6,6 +6,7 @@
 package gestionecinema;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 /**
  *
@@ -17,8 +18,15 @@ public class GestioneCinema {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws FileNotFoundException {
-        Catalogo c = new Catalogo();
+       Catalogo c = new Catalogo();
         c.riempiCatalogo();
+        Interfaccia i=new Interfaccia();
+        i.setVisible(true);
+        String []s=new String[c.listaTitoliFilm().size()];
+        for (int j=0;j<c.listaTitoliFilm().size();j++){
+            s[j]=(String) (c.listaTitoliFilm().get(j));
+        } 
+        i.listaTitoli.setListData(s);
     }
     
 }
