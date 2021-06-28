6d4900b804daab7e78a9a8190e33be591a5feb43
matte <matte@DESKTOP-RS61TL9.station>
2021-06-28 15:54:45 +0200

    da risolvere

--- src/gestionecinema/Biglietto.java
+++ src/gestionecinema/Biglietto.java
@@ -9,7 +9,7 @@ import javax.swing.JOptionPane;
 
 public class Biglietto {
     private Proiezione scelta=new Proiezione();
-    
+    private static int nTotBiglietti = 0;
     //costruttori
     public Biglietto(Proiezione scelta) {
         this.scelta = scelta;
@@ -36,13 +36,14 @@ public class Biglietto {
             for(int i=0;i<scelta.getSala_p().getNf();i++){ // scorriamo le file
                 if(scelta.getSala_p().liberiFila(i)>=nb){ //vediamo se nella fila ci sono abbastanza posti
                     for (int j=0;j<scelta.getSala_p().getGf();j++){ //scorriamo i sedili
-                        if(scelta.getSala_p().getPosti()[i][j].getOccupato()==false && n<nb){ //se il sedile è non occupato e non abbiamo ancora stampato tutti i biglietti
+                        if(scelta.getSala_p().getPosti()[i][j].getOccupato()==false && n<nb){//se il sedile è non occupato e non abbiamo ancora stampato tutti i biglietti
+                            nTotBiglietti++;
                             f.write("Biglietto numero: "+(n+1)+"\n");//System.out.println(scelta.getFilm_p().getTitolo()+"  ora: "+scelta.getOrario_p().toString()+"\n"+scelta.getSala_p().getNumero()+" Posto: "+scelta.getSala_p().getPosti()[i][j]);
                             f.write("Film: "+scelta.getFilm_p().getTitolo()+"\nOrario: "+scelta.getOrario_p().toString()+"\nSala: "+scelta.getSala_p().getNumero()+"\nPosto: "+scelta.getSala_p().getPosti()[i][j]);
                             f.write("\n\n");
-                            output.write("Biglietto numero: "+(n+1)+"\n");//System.out.println(scelta.getFilm_p().getTitolo()+"  ora: "+scelta.getOrario_p().toString()+"\n"+scelta.getSala_p().getNumero()+" Posto: "+scelta.getSala_p().getPosti()[i][j]);
+                            output.write("Biglietto numero: "+(nTotBiglietti)+"\n");//System.out.println(scelta.getFilm_p().getTitolo()+"  ora: "+scelta.getOrario_p().toString()+"\n"+scelta.getSala_p().getNumero()+" Posto: "+scelta.getSala_p().getPosti()[i][j]);
                             output.write("Film: "+scelta.getFilm_p().getTitolo()+"\nOrario: "+scelta.getOrario_p().toString()+"\nSala: "+scelta.getSala_p().getNumero()+"\nPosto: "+scelta.getSala_p().getPosti()[i][j]);
-                            output.write("\n");
+                            output.write("\n\n");
                             scelta.getSala_p().occupaPosto(scelta.getSala_p().getPosti()[i][j]); // rendiamo il posto occupato
                             n++;// aggiorniamo i biglietti stampati
                         }
