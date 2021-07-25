71d834e06290f464dd66719d1fca595ea1031498
tittioAngel <44965659+tittioAngel@users.noreply.github.com>
2021-07-25 19:53:22 +0200

    Good Good i biglietti stampano giusti, serve aggiornare lo storico biglietti

--- src/gestionecinema/Biglietto.java
+++ src/gestionecinema/Biglietto.java
@@ -38,27 +38,25 @@ public class Biglietto {
         File storico_biglietti=new File("src\\gestionecinema\\storico_biglietti.txt");
         Writer output = null;
         output = new BufferedWriter(new FileWriter(storico_biglietti, true));
-        
+        int n=0;// tiene conto dei posti assegnati 
         int fila = scelta.getSala_p().trovaFila(nb);
-        if(fila == -1){
+        Posto p=new Posto();
+        
+        if( scelta.getSala_p().trovaFila(nb) == -1){  // vediamo se ci sono abbastanza posti consecutivi
             JOptionPane.showMessageDialog(null, "La sala "+scelta.getSala_p().getNumero()+" alle ore "+scelta.getOrario_p().toString()+" non ha abbastanza POSTI LIBERI consecutivi","ATTENZIONE!!",ERROR_MESSAGE);
-        }
-        else{
-            boolean stampa= true;
-            int i=0;
-            while(stampa){
-                if(scelta.getSala_p().getPosti()[fila][i].getOccupato()==false){
-                    Posto p = new Posto(fila,i);
-                    posto_assegnato = p;
-                    for(int n=0;n<nb;n++){
-                        f.write("Biglietto numero: "+(n+1)+"\n");
-                        f.write(this.toString());
-                        i++;
+        }else{
+            for(int i=0;i<scelta.getSala_p().getSf()&& n<nb;i++){
+                    //p=scelta.getSala_p().getPosti()[fila][i];
+                    if(!scelta.getSala_p().getPosti()[fila][i].getOccupato()){
+                        posto_assegnato=scelta.getSala_p().getPosti()[fila][i];
+                        f.write("Biglietto n: "+(n+1)+"\n"+"Film: "+scelta.getFilm_p().getTitolo()+"  Sala: "+scelta.getSala_p().getNumero()+" Ora:"+scelta.getOrario_p()+"\n Posto: "+posto_assegnato.toString());
+                        f.write("\n\n");
+                        n++;
+                        scelta.getSala_p().getPosti()[fila][i].setOccupato(true);
+                        System.out.println(posto_assegnato);
+                        System.out.println(posto_assegnato.getOccupato());
                     }
-                    stampa=true; 
-                }
-                i++;
-                
+                    
             }
         }
         f.close();
